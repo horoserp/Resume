7000f8c6bdaf65e7463d8f8d828b283eab212f85
Robert Horosewski <horoserp@yahoo.com>
2023-01-14 17:16:45 -0500

    Added print button

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+// Print Function
+function printResume() {
+    alert('Please make sure to enable printing "Background graphics" in "More settings" to ensure proper printing!');
+    window.print();
+}
