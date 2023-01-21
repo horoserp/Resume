5f78799f0b4c518b57fc410d6f48ae148dd070a7
Robert Horosewski <horoserp@yahoo.com>
2023-01-21 17:30:52 -0500

    Began implementation of blinking text

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,44 @@
+var clr1;
+clr1 = setInterval(function() {
+  flash('blinking', 'green', 'red')
+}, 1000);
+
 // Print Function
 function printResume() {
+  // Stop the blinking text
+  clearInterval(clr1);
+  clr1 = setInterval(function() {
+    flash('blinking', 'black', 'black')
+  }, 1000);
+  const myTimeout = setTimeout(myGreeting, 1000);
+}
+
+// Make select text blink
+function flash(el, c1, c2) {
+  var text = document.getElementById(el);
+  text.style.color = (text.style.color == c2) ? c1 : c2;
+  setTimeout
+}
+
+function myGreeting() {
+  // Display pop-up alert giving recommended print settings
   alert('Please make sure to enable printing "Background graphics" in "More settings" to ensure proper printing!');
+  // Hide the scrolling text
   textAnimation = document.getElementById('scroll-text');
   textAnimation.style.display = "none";
+  // Display standard text
   textStop = document.getElementById('scroll-text2');
   textStop.style.display = "block";
   textStop.style.margin = "0 0 0 60px";
+  // Open print dialog
   window.print();
+  // Hide standard text and shot scrolling text again
   textStop.style.display = "none";
   textAnimation.style.display = "block";
+  // Restart blinking text
+  clearInterval(clr1);
+  clr1 = setInterval(function() {
+    flash('blinking', 'red', 'green')
+  }, 1000);
+
 }
