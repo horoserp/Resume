53837f66de7f141688bc847f0b3b4a72cd6d2020
Robert Horosewski <horoserp@yahoo.com>
2025-01-15 22:15:24 -0500

    Update: Remove photo. Remove <hr>. Remove all color from resume. Reorder sections. Remove multiple 'I' statements from Objective.

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,20 @@
-$(document).ready(()=> {
+$(document).ready(() => {
   // Start the element blinking
   var setBlinking;
   setBlinking = setInterval(() => {
-    flash('blinking', '#00cc00', 'black')
+    flash("blinking", "black", "black");
   }, 500);
 
   // Make select element change colors
   function flash(element, color1, color2) {
     var text = document.getElementById(element);
-    text.style.color = (text.style.color == color2) ? color1 : color2;
+    text.style.color = text.style.color == color2 ? color1 : color2;
   }
 
   // Print Resume
 
   // When the print button is clicked...
-  $("#print-button").click(()=> {
+  $("#print-button").click(() => {
     // Stop the blinking time and...
     clearInterval(setBlinking);
     // Set the text color to black...
@@ -23,11 +23,13 @@ $(document).ready(()=> {
     $("#scroll-text").css("display", "none");
     // Display the standard text and set an appropriate margin...
     $("#scroll-text2").css({
-      "display": "block",
-      "margin": "0 0 0 65px"
+      display: "block",
+      margin: "0 0 0 65px",
     });
     // Display a pop-up alert stating recommended print settings...
-    alert('Please make sure to enable printing "Background graphics" in "More settings" to ensure proper printing!');
+    alert(
+      'Please make sure to enable printing "Background graphics" in "More settings" to ensure proper printing!'
+    );
     // Open the print dialog box...
     window.print();
     // Hide standard text...
@@ -37,7 +39,7 @@ $(document).ready(()=> {
     // And restart the blinking text
     clearInterval(setBlinking);
     setBlinking = setInterval(() => {
-      flash('blinking', '#00cc00', 'black')
+      flash("blinking", "black", "black");
     }, 500);
   });
 });
